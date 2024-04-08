f007269e5de651820a4f8e1d4a7e8ea62a33047c
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2024-04-08 11:45:47 -0700

    attempting to get live page to render by changing build script and removing homepage from package.json

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,6 @@ function About() {
   return (
     <div style={containerStyle}>
       <div style={imageContainerStyle}>
-        {/* Placeholder for your photo */}
         <img src="../src/assets/images/IMG_3807.JPG" alt="Picture of Somphang Chanleuxay" style={imageStyle} />
       </div>
       <div style={textContainerStyle}>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyPortfoliowithReact/', // Replace 'repository-name' with your actual repository name
+  base: '/', 
   plugins: [react()]
 });
